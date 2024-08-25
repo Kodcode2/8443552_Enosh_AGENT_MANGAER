4f00f91f8d52727ddc3aa900b16020a966965ca0
Shai Rainer <shaira@std.hac.ac.il>
2024-08-25 11:20:20 +0300

    add MVC

---
+++ Mvc/AgentsClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgentsClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
