2c7796f877e5af15b91d74167511e86c01be2128
Shai Rainer <shaira@std.hac.ac.il>
2024-08-26 12:15:06 +0300

    new server

--- Mvc/AgentsClient/obj/Debug/net8.0/AgentsClient.AssemblyInfo.cs
+++ Mvc/AgentsClient/obj/Debug/net8.0/AgentsClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgentsClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d9fb38efada56fdc071965db443011941d81467")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7091d82d69371838463c833bb0403d399815ac96")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgentsClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgentsClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Rest/AgentsRest/obj/Debug/net8.0/AgentsRest.AssemblyInfo.cs
+++ Rest/AgentsRest/obj/Debug/net8.0/AgentsRest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgentsRest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d9fb38efada56fdc071965db443011941d81467")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7091d82d69371838463c833bb0403d399815ac96")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgentsRest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgentsRest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
